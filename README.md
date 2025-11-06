f7deab4074f952c48c7f0f8abeacc6ff206f2191
Vardo Ternos <tvardero@gmail.com>
2025-11-07 00:18:28 +0200

    Add mod from template

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# ta.RainWorldMods
+
+## Build
+
+Prerequsites:
+- .NET SDK version 9.0 or newer
+- RainWorld installed
+- Knowledge of modern C# (version 11+)
+- Knowledge of nullable reference types
+- Knowledge of RainWorld modding
+
+Steps:
+1. Go to the RainWorld installation folder
+2. Copy following `.dll` files from RainWorld folder to `dll/` folder in repository:
+   - `Rain World/BepInEx/core/BepInEx.dll`
+   - `Rain World/BepInEx/utils/PUBLIC-Assembly-CSharp.dll`
+   - `Rain World/BepInEx/plugins/HOOKS-Assembly-CSharp.dll`
+   - `Rain World/RainWorld_Data/Managed/UnityEngine.dll`
+   - `Rain World/RainWorld_Data/Managed/UnityEngine.CoreModule.dll`
+3. Run `dotnet build`
---
+++ src/ta.FirstMod/TemplateMod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using JetBrains.Annotations;
+using UnityEngine;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace TemplateMod;
+
+[BepInPlugin("AuthorName.ModName", "Mod Name", "1.0.0")]
+public class TemplateMod : BaseUnityPlugin
+{
+    private bool _initialized;
+
+    [UsedImplicitly]
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private void PlayerOnUpdate(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        orig(self, eu); //Always call original code, either before or after your code, depending on what you need to achieve
+
+        self.slugcatStats.runspeedFac += 0.01f;
+        Debug.Log($"Player {self.playerState.playerNumber} feels like zooming.");
+        Debug.Log($"Player {self.playerState.playerNumber}'s run speed: {self.slugcatStats.runspeedFac}");
+    }
+
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        if (_initialized) return;
+
+        try
+        {
+            _initialized = true;
+
+            //Your hooks go here
+            On.Player.Update += PlayerOnUpdate;
+        }
+        catch (Exception ex) { Logger.LogError(ex); }
+    }
+}
